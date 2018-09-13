19eeff452087e2182227a6181f626ed11798f732
Liam Dale <liamdale15@gmail.com>
2018-09-14 09:38:29 +1000

    Models and Clean DB updates

--- CRC/CarRentalCompany/models.py
+++ CRC/CarRentalCompany/models.py
@@ -4,21 +4,24 @@ from datetime import *
 
 # Create your models here.
 
-##Process
-# Added to helloworld/settings
-# python manage.py makemigrations CRC
-# python manage.py sqlmigrate CRC 00##
-# python manage.py migrate
-# python manage.py shell ....
-    #from CRC.models import Car, Order
-    #from django.utils import timezone
-    #c = Car(car_name = "", car_bodytype = "")
-    #c.save()
-    #Car.objects.get(pk=1)
-    #c.order_set.create(order_location = "", order_date=timezone.now())
-    #c.order_set.all()
+'''
+PROCESS ---
+Added to helloworld/settings
+
+python manage.py makemigrations CRC
+python manage.py sqlmigrate CRC 00##
+python manage.py migrate
 
 
+python manage.py shell ....
+    from CRC.models import Car, Order
+    from django.utils import timezone
+    c = Car(car_name = "", car_bodytype = "")
+    c.save()
+    Car.objects.get(pk=1)
+    c.order_set.create(order_location = "", order_date=timezone.now())
+    c.order_set.all()
+'''
 
 class Car(models.Model):
     car_makename = models.CharField(max_length = 128, default = "null")
@@ -37,42 +40,43 @@ class Car(models.Model):
     car_wheelbase = models.CharField(max_length = 128, default = "null")
     car_bodytype = models.CharField(max_length = 128, default = "null")
     def __str__(self):
-        return self.car_make_name
+        return self.car_makename + ": " + self.car_model
 
 class Store(models.Model):
     store_name = models.CharField(max_length = 128, default = "null")
     store_address = models.CharField(max_length = 128, default = "null")
-    store_phone = models.IntegerField(default = -1)
+    store_phone = models.BigIntegerField(default = -1)
     store_city = models.CharField(max_length = 128, default = "null")
     store_state = models.CharField(max_length = 128, default = "null")
     def __str__(self):
-        return self.store_location
+        return self.store_name
 
 class User(models.Model):
     user_name = models.CharField(max_length = 32, default = "null")
     user_phone = models.IntegerField(default = -1)
     user_address = models.CharField(max_length = 32, default = "null")
-    user_birthday = models.DateTimeField('user birthday', default=datetime.now, blank=True)
+    user_birthday = models.DateField('user birthday', default=datetime.today, blank=True)
     user_occupation = models.CharField(max_length = 32, default = "null")
     user_gender = models.CharField(max_length = 32, default = "null")
-    email = models.EmailField(default = -1)
-    password = models.CharField(max_length = 128, default = "null")
+    user_password = models.CharField(max_length = 128, default = "null")
     def __str__(self):
-        return self.name
+        return self.user_name
 
 class Order(models.Model):
     car_id = models.ForeignKey(Car, on_delete = models.CASCADE) 
     customer_id = models.ForeignKey(User, on_delete = models.CASCADE)
-    order_create_date = models.DateTimeField('order create date', default=datetime.now, blank=True)
+    order_create_date = models.DateField('order create date', default=datetime.today, blank=True)
     order_pickup_store_id = models.ForeignKey(Store, related_name = 'pickup_store_order_set', on_delete = models.CASCADE)
-    order_pickup_date = models.DateTimeField('order pickup date', default=datetime.now, blank=True)
+    order_pickup_date = models.DateField('order pickup date', default=datetime.today, blank=True)
     order_return_store_id = models.ForeignKey(Store, related_name = 'return_store_order_set', on_delete = models.CASCADE)
-    order_return_date = models.DateTimeField('order return date', default=datetime.now, blank=True)
+    order_return_date = models.DateField('order return date', default=datetime.today, blank=True)
     def __str__(self):
-        return self.order_location + ": " + self.order_date.strftime("%d-%b-%y %I:%M:%S%p")
-    
+        return self.order_pickup_store_id.store_name + ": " + self.order_pickup_date.strftime("%d-%b-%y")
+
 '''
-p = Car(car_makename = row['Car_MakeName'],
+
+c = Car(id = row['Car_ID'],
+    car_makename = row['Car_MakeName'],
     car_model = row['Car_Model'],
     car_series = row['Car_Series'],
     car_series_year = row['Car_SeriesYear'],
@@ -86,4 +90,30 @@ p = Car(car_makename = row['Car_MakeName'],
     car_bodytype = row['Car_BodyType'],
     car_drive = row['Car_Drive'],
     car_wheelbase = row['Car_Wheelbase'])
+
+s = Store(id = row['Store_ID'],
+    store_name = row['Store_Name'],
+	store_address = row['Store_Address'],
+	store_phone = int(row['Store_Phone'].replace("-", "").replace(" ", "")[-10:]),
+	store_city = row['Store_City'],
+	store_state = row['Store_State_Name'])
+
+u = User(id = row['Customer_ID'],
+    user_name = row['Customer_Name'],
+	user_phone = int(row['Customer_Phone'].replace("-", "")),
+	user_address = row['Customer_Address'],
+	user_birthday = date((int(row['Customer_Birthday'].split("-")[2]) + 1900), int(row['Customer_Birthday'].split("-")[1]), int(row['Customer_Birthday'].split("-")[0])),
+	user_occupation = row['Customer_Occupation'],
+	user_gender = row['Customer_Gender'],
+	user_password = row['Customer_Password'])
+    
+o = Order(id = row['Order_ID'],
+    car_id = Car.objects.get(pk = row['Car_ID']),
+    customer_id = User.objects.get(pk = row['Customer_ID']),
+    order_create_date = date((int(row['Order_Create_Date'].split("-")[2]) + 2000), int(row['Order_Create_Date'].split("-")[1]), int(row['Order_Create_Date'].split("-")[0])),
+    order_pickup_store_id = Store.objects.get(pk = row['Order_Pickup_Store']),
+    order_pickup_date = date((int(row['Order_Pickup_Date'].split("-")[2]) + 2000), int(row['Order_Pickup_Date'].split("-")[1]), int(row['Order_Pickup_Date'].split("-")[0])),
+    order_return_store_id = Store.objects.get(pk = row['Order_Return_Store']),
+    order_return_date = date((int(row['Order_Return_Date'].split("-")[2]) + 2000), int(row['Order_Return_Date'].split("-")[1]), int(row['Order_Return_Date'].split("-")[0])))
+
 '''
