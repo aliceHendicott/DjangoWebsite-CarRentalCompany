5f343c99c9813f4a601d28f767644042dc4ff1b3
Alice Hendicott <alice.hendicott@gmail.com>
2018-09-20 16:33:37 +1000

    added query

--- CRC/CarRentalCompany/custom_sql.py
+++ CRC/CarRentalCompany/custom_sql.py
@@ -27,6 +27,19 @@ def seasonal_cars_preview():
     results = Car.objects.raw(query)
     return results
 
+def seasonal_cars():
+    query = '''SELECT CarRentalCompany_car.*, count(CarRentalCompany_order.car_id_id) as number_of_orders
+                FROM CarRentalCompany_car
+                LEFT JOIN CarRentalCompany_order
+                ON (CarRentalCompany_order.car_id_id = CarRentalCompany_car.id)
+                WHERE
+					MONTH(CarRentalCompany_order.order_pickup_date)=2 AND YEAR(CarRentalCompany_order.order_pickup_date)=2007
+                GROUP BY
+	                CarRentalCompany_car.id
+                ORDER BY number_of_orders DESC'''
+    results = Car.objects.raw(query)
+    return results
+
 def store_activity_preview():
     query = ('''SELECT CarRentalCompany_store.*, count(CarRentalCompany_order.order_pickup_store_id_id) as number_of_orders
                 FROM CarRentalCompany_store
