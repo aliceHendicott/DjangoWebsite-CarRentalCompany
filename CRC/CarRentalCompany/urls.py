7f03824d553f58cd0b6374490de6d8f9e2f9cbd9
Liam Dale <liamdale15@gmail.com>
2018-10-08 23:27:11 +1000

    AJAX Updates

--- CRC/CarRentalCompany/urls.py
+++ CRC/CarRentalCompany/urls.py
@@ -44,14 +44,20 @@ urlpatterns = [
 
     ## Reports
     path('reports/dashboard/', views_reports.dashboard, name='reports_dashboard'),
+    re_path(r'^ajax/json_dashboard_context/$', views_reports.json_dashboard_context, name='json_dashboard_context'),
     #  Car reports
     path('reports/cars/seasonal/', views_reports.cars_seasonal, name='reports_cars_seasonal'),
+    re_path(r'^ajax/json_cars_seasonal_context/$', views_reports.json_cars_seasonal_context, name='json_cars_seasonal_context'),
     path('reports/cars/inactive/', views_reports.cars_inactive, name='reports_cars_inactive'),
+    re_path(r'^ajax/json_cars_inactive_context/$', views_reports.json_cars_inactive_context, name='json_cars_inactive_context'),
     #  Store reports
     path('reports/stores/activity/', views_reports.store_activity, name='reports_store_activity'),
+    re_path(r'^ajax/json_store_activity_context/$', views_reports.json_store_activity_context, name='json_store_activity_context'),
     path('reports/stores/parking/', views_reports.store_parking, name='reports_store_parking'),
+    re_path(r'^ajax/json_store_parking_context/$', views_reports.json_store_parking_context, name='json_store_parking_context'),
     #  Customer reports
     path('reports/customers/demographics/', views_reports.customer_demographics, name='reports_customer_demographics'),
+    re_path(r'^ajax/json_customer_demographics_context/$', views_reports.json_customer_demographics_context, name='json_customer_demographics_context'),
 
     ## TEST
     path('connection/', TemplateView.as_view(template_name ='CarRentalCompany/../templates/registration/login.html')),
--- CRC/CarRentalCompany/views_reports.py
+++ CRC/CarRentalCompany/views_reports.py
@@ -10,27 +10,30 @@ from .forms import RecommendForm
 from .custom_sql import *
 from .recommendation import handle_recommendation
 from django.contrib.auth import (authenticate, login, get_user_model, logout)
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 
 # ------- REPORTS ------ #
 
 ## Supporting
+
+# Authentication
 def is_management(request):
     if request.user.is_authenticated:
         user_profile = request.user.userprofile
         customer = user_profile.is_customer
         floor_staff = user_profile.is_floorStaff
         if not customer and not floor_staff:
-            return True
+           return True
     return False
 
-
+# Graphs
 def cars_seasonal_graph(data):
     graphdata = []
     for car in data:
         graphdata.append([car.car_makename, car.number_of_orders])
     return drawGraph('bar', 'cars_seasonal', graphdata)
-
 def cars_inactive_graph():
     graphdata = [['VW Golf', 5],
                  ['Getz', 12],
@@ -39,7 +42,6 @@ def cars_inactive_graph():
                  ['Lotus x', 15],
                  ['Tesla', 16]]
     return drawGraph('horizBar', 'cars_inactive', graphdata)
-
 def store_parking_graph():
     graphdata = [['Brisbane', 16],
                  ['Warrnambool', 15],
@@ -47,13 +49,11 @@ def store_parking_graph():
                  ['Gold Coast', 13],
                  ['Adelaide', 5]]
     return drawGraph('horizBar', 'store_parking', graphdata)
-
 def store_activity_graph(data):
     graphdata = []
     for store in data:
         graphdata.append([store.store_city, store.total_activity])
     return drawGraph('pie', 'store_activity', graphdata)
-
 def customer_demographics_graph():
     graphdata = [['Female 18-25', 1],
                  ['Male 35-45', 3],
@@ -67,82 +67,148 @@ def customer_demographics_graph():
 ' SPRINT 1
 ' The following are sprint 1:
 '''
-
+##### Reports Dashboard #####
+def dashboard_context(dates = 1, limit = 5):
+    seasonal_cars = Car.top_cars(limit)
+    store_activity = Store.store_activity(limit)
+    car_parks = Car.inactive_cars()
+    context =  {'seasonal_cars': seasonal_cars,
+                'store_activity': store_activity,
+                'cars_seasonal_graph': cars_seasonal_graph(seasonal_cars),
+                'cars_inactive_graph': cars_inactive_graph(),
+                'store_parking_graph': store_parking_graph(),
+                'store_activity_graph': store_activity_graph(store_activity),
+                'customer_demographics_graph': customer_demographics_graph()}
+    return context
+def json_dashboard_context(request):
+    dates = (request.GET.get('start_date', None), request.GET.get('end_date', None))
+    data_rendered = {
+        'html_response': render_to_string("CarRentalCompany/Includes/reports_dashboard_content.html", dashboard_context(dates=dates, limit=10))
+    }
+    return JsonResponse(data_rendered)
 def dashboard(request):
     if is_management(request):
-        seasonal_cars = Car.top_cars(5)
-        store_activity = Store.store_activity(5)
-        car_parks = Car.inactive_cars()
         return render(request,
-                        'CarRentalCompany/reports_dashboard.html',
-                        {'seasonal_cars': seasonal_cars,
-                        'store_activity': store_activity,
-                        'cars_seasonal_graph': cars_seasonal_graph(seasonal_cars),
-                        'cars_inactive_graph': cars_inactive_graph(),
-                        'store_parking_graph': store_parking_graph(),
-                        'store_activity_graph': store_activity_graph(store_activity),
-                        'customer_demographics_graph': customer_demographics_graph()})
-    else:
-        return redirect('index')
+                      'CarRentalCompany/reports_dashboard.html',
+                      dashboard_context())
+    return redirect('index')
 
+
+
+##### Seasonal Cars Report #####
+def cars_seasonal_context(dates=(1,2), limit = 5):
+    seasonal_cars_results = seasonal_cars()
+    context =  {'cars_list': Car.objects.all(),
+                'seasonal_cars':  seasonal_cars_results,
+                'cars_seasonal_graph': cars_seasonal_graph(seasonal_cars_results)}
+    return context
+def json_cars_seasonal_context(request):
+    dates = (request.GET.get('start_date', None), request.GET.get('end_date', None))
+    data_rendered = {
+        'html_response': render_to_string("CarRentalCompany/Includes/reports_cars_seasonal_content.html", cars_seasonal_context(dates))
+    }
+    return JsonResponse(data_rendered)
 def cars_seasonal(request):
     if is_management(request):
-        seasonal_cars_results = seasonal_cars()
         return render(request,
-                        'CarRentalCompany/reports_cars_seasonal.html',
-                        {'cars_list': Car.objects.all(),
-                        'seasonal_cars':  seasonal_cars_results,
-                        'cars_seasonal_graph': cars_seasonal_graph()})
-    else:
-        return redirect('index')
+                      'CarRentalCompany/reports_cars_seasonal.html',
+                      cars_seasonal_context())
+    return redirect('index')
 
+
+
+##### Inactive Cars Report #####
+def cars_inactive_context(dates=(1,2), limit = 5):
+    inactive_car_results = inactive_cars()
+    context =  {'cars_list': Car.objects.all(),
+                'inactive_cars': inactive_car_results,
+                'cars_inactive_graph': cars_inactive_graph()}
+    return context
+def json_cars_inactive_context(request):
+    dates = (request.GET.get('start_date', None), request.GET.get('end_date', None))
+    data_rendered = {
+        'html_response': render_to_string("CarRentalCompany/Includes/reports_cars_inactive_content.html", cars_inactive_context(dates))
+    }
+    return JsonResponse(data_rendered)
 def cars_inactive(request):
     if is_management(request):
-        inactive_car_results = inactive_cars()
         return render(request,
-                        'CarRentalCompany/reports_cars_inactive.html',
-                        {'cars_list': Car.objects.all(),
-                        'inactive_cars': inactive_car_results,
-                        'cars_inactive_graph': cars_inactive_graph()})
-    else:
-        return redirect('index')
+                      'CarRentalCompany/reports_cars_inactive.html',
+                      cars_seasonal_context())
+    return redirect('index')
 
+
+
+##### Store Activity Report #####
+def store_activity_context(dates=(1,2), limit = 5):
+    inactive_car_results = inactive_cars()
+    locations = []
+    for store in Store.objects.all():
+        locations.append([eval(store.store_latitude), eval(store.store_longitude), store.store_name])
+    store_results = store_activity_query()
+    context =  {'stores_list': Store.objects.all(),
+                'location_maps': locations,
+                'store_results': store_results,
+                'store_activity_graph': store_activity_graph(store_results)}
+    return context
+def json_store_activity_context(request):
+    dates = (request.GET.get('start_date', None), request.GET.get('end_date', None))
+    data_rendered = {
+        'html_response': render_to_string("CarRentalCompany/Includes/reports_store_activity_content.html", store_activity_context(dates))
+    }
+    return JsonResponse(data_rendered)
 def store_activity(request):
     if is_management(request):
-        locations = []
-        for store in Store.objects.all():
-            locations.append([eval(store.store_latitude), eval(store.store_longitude), store.store_name])
-        store_results = store_activity_query()
         return render(request,
-                        'CarRentalCompany/reports_store_activity.html',
-                        {'stores_list': Store.objects.all(),
-                        'location_maps': locations,
-                        'store_results': store_results,
-                        'store_activity_graph': store_activity_graph()})
-    else:
-        return redirect('index')
+                      'CarRentalCompany/reports_store_activity.html',
+                      store_activity_context())
+    return redirect('index')
 
+
+
+##### Store Parking Report #####
+def store_parking_context(dates=(1,2), limit = 5):
+    results = store_parking_query()
+    context =  {'queried_stores': results,
+                'stores': Store.objects.all(),
+                'store_parking_graph': store_parking_graph()}
+    return context
+def json_store_parking_context(request):
+    dates = (request.GET.get('start_date', None), request.GET.get('end_date', None))
+    data_rendered = {
+        'html_response': render_to_string("CarRentalCompany/Includes/reports_store_parking_content.html", store_parking_context(dates))
+    }
+    return JsonResponse(data_rendered)
 def store_parking(request):
     if is_management(request):
-        results = store_parking_query()
         return render(request,
-                        'CarRentalCompany/reports_store_parking.html',
-                        {'queried_stores': results,
-                        'stores': Store.objects.all(),
-                        'store_parking_graph': store_parking_graph()})
-    else:
-        return redirect('index')
+                      'CarRentalCompany/reports_store_parking.html',
+                      store_parking_context())
+    return redirect('index')
 
+
+
+
+##### Customer Demographics Report #####
+def customer_demographics_context(dates=(1,2), limit = 5):
+    results = customer_demographics_query()
+    context =  {'users_list': User.objects.all(),
+                'results': results,
+                'customer_demographics_graph': customer_demographics_graph()}
+    return context
+def json_customer_demographics_context(request):
+    dates = (request.GET.get('start_date', None), request.GET.get('end_date', None))
+    data_rendered = {
+        'html_response': render_to_string("CarRentalCompany/Includes/reports_customer_demographics_content.html", customer_demographics_context(dates))
+    }
+    return JsonResponse(data_rendered)
 def customer_demographics(request):
     if is_management(request):
         results = customer_demographics_query()
         return render(request,
-                        'CarRentalCompany/reports_customer_demographics.html',
-                        {'users_list': User.objects.all(),
-                        'results': results,
-                        'customer_demographics_graph': customer_demographics_graph()})
-    else:
-        return redirect('index')
+                      'CarRentalCompany/reports_customer_demographics.html',
+                      customer_demographics_context())
+    return redirect('index')
 
 
 
